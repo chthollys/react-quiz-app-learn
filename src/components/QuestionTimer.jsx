ac9b2ebc516e80bab78805647e4927c154d0c182
chthollys <jason130105@gmail.com>
2025-06-10 16:53:08 +0700

    remove log
    Signed-off-by: chthollys <jason130105@gmail.com>

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -4,7 +4,7 @@ function QuestionTimer({ timeout, onTimeout, mode }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
-    console.log("setting timeout");
+    // console.log("setting timeout");
     const timer = setTimeout(onTimeout, timeout);
 
     return () => {
